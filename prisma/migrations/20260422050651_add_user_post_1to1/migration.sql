ac2d26f2d615f43cc8efb371264566f641cf9658
Danielo Devs <brandon.omd@gmail.com>
2026-04-21 23:08:14 -0600

    feat(schema):_user_post_1to1_relation_with_unique_foreign_key

---
+++ prisma/migrations/20260422050651_add_user_post_1to1/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Post_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
